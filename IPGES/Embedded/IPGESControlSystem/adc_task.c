2214521fd172c2619121e61b5361e8b2f9d7697a
Unknown <jiahan.jim.liu@gmail.com>
2018-04-04 13:46:29 -0500

    Ben Day

--- IPGES/Embedded/IPGESControlSystem/adc_task.c
+++ IPGES/Embedded/IPGESControlSystem/adc_task.c
@@ -70,10 +70,15 @@ void clearAdcData (AdcSS0Data *data);
 void setAdcData (AdcSS0Data *data);
 int sqrt(int input);
 int undo_signal_conditioning_load_vrms(int input);
+int undo_signal_conditioning_load_irms(int input);
+int undo_signal_conditioning_dist_vrms(int input);
+int undo_signal_conditioning_dist_irms(int input);
 int scaling(int input, int *boundary, int *scale);
 
-#define scb_mean_load_vrms 2022
-#define scb_mean_load_irms 2867
+#define scb_mean_load_vrms 2022 //Power Supply 4.131
+#define scb_mean_load_irms 2891 //
+#define scb_mean_dist_vrms 0000 //Power Supply 4.131
+#define scb_mean_dist_irms 0000 //
 #define scb_factor 11
 #define transformer_factor 5
 
@@ -83,7 +88,9 @@ AdcSS0Data *adcRawSS0Input;
 uint16_t adc_ss0_index = 0;
 static uint32_t load_v_rms;
 static uint32_t load_i_rms;
-int test; 
+static uint32_t dist_v_rms;
+static uint32_t dist_i_rms;
+//int test; 
 
 xSemaphoreHandle ss0Full;
 
@@ -94,8 +101,12 @@ static void ADCTask(void *pvParameters)
 		int timeOut = 0xffff;
 		int result_load_vrms = 0;
 		int result_load_irms = 0;
+		int result_dist_vrms = 0;
+		int result_dist_irms = 0;
 		int sum_load_vrms = 0;
 		int sum_load_irms = 0;
+		int sum_dist_vrms = 0;
+		int sum_dist_irms = 0;
 		AdcSS0Data shifted_adc;
 	
 		// Print the current loggling LED and frequency.
@@ -112,19 +123,28 @@ static void ADCTask(void *pvParameters)
 						//PE1
 						shifted_adc.PE1 = adcRawSS0Input[i].PE1 - scb_mean_load_irms; //5.4 -> 2.31
 						sum_load_irms += shifted_adc.PE1 * shifted_adc.PE1; 
-						//UARTprintf("%d,", (adcRawSS0Input[i].PE1* 3300) / 4095);
+						//PE2
+						shifted_adc.PE2 = adcRawSS0Input[i].PE2 - scb_mean_dist_vrms;
+						sum_dist_vrms += shifted_adc.PE2 * shifted_adc.PE2;
+						//PE3
+						shifted_adc.PE3 = adcRawSS0Input[i].PE2 - scb_mean_dist_irms;
+						sum_dist_irms += shifted_adc.PE3 * shifted_adc.PE3; 
+						
+						//UARTprintf("%d,", (adcRawSS0Input[i].PE2* 3300) / 4095);
 						//UARTprintf("%d,", adcRawSS0Input[i].PE0);
 						//UARTprintf("%d,",shifted_adc.PE0);
           }
           sum_load_vrms /= ARRAY_SIZE;
 					sum_load_irms /= ARRAY_SIZE;
+					
           sum_load_vrms = sqrt(sum_load_vrms);
 					sum_load_irms = sqrt(sum_load_irms);
+					
 					result_load_vrms = ((sum_load_vrms) * 3300) / 4095;
 					result_load_irms = ((sum_load_irms) * 3300) / 4095;
+					
 					load_v_rms = undo_signal_conditioning_load_vrms(result_load_vrms);
-					test = result_load_vrms;
-					load_i_rms = result_load_irms;	
+			 		load_i_rms = result_load_irms;	
 					
         }
     }
@@ -137,9 +157,31 @@ int undo_signal_conditioning_load_vrms(int input) {
 	return real_value;   
 }
 
+int undo_signal_conditioning_load_irms(int input) {
+	int boundaries [28] = {58,62,65,65,68,68,71,71,74,78,81,87,91,97,103,124,153,182,221,263,323,384,440,503,563,624,660,685};
+	int scale [28]  = {440, 490, 470, 490, 520, 550, 580, 610, 640, 670, 715, 760, 820, 865, 930, 1120, 1330, 1560, 1840, 2140, 2510, 2860, 3230, 3620, 4045, 4490, 4970, 5530};
+	int real_value = scaling(input, boundaries, scale);
+	return real_value;   
+}
+
+int undo_signal_conditioning_dist_vrms(int input) {
+	int boundaries [28] = {20, 23, 26, 29, 36, 39, 45, 52, 55, 62, 68, 74, 84, 91, 97, 108, 137, 157, 173, 192, 215, 224, 244, 263, 279, 295, 308, 326};
+	int scale [28]  = {65,465,2265,3300,3611,4169,4340,4396,4778,4800,4929,5064,4904,5010,5134,5564,5145,5133,5225,5140,5130,5281,5241,5197,5211,5220,5211,5220};
+	int real_value = scaling(input, boundaries, scale);
+	return real_value;   
+}
+
+int undo_signal_conditioning_dist_irms(int input) {
+	int boundaries [28] = {58,62,65,65,68,68,71,71,74,78,81,87,91,97,103,124,153,182,221,263,323,384,440,503,563,624,660,685};
+	int scale [28]  = {440, 490, 470, 490, 520, 550, 580, 610, 640, 670, 715, 760, 820, 865, 930, 1120, 1330, 1560, 1840, 2140, 2510, 2860, 3230, 3620, 4045, 4490, 4970, 5530};
+	int real_value = scaling(input, boundaries, scale);
+	return real_value;   
+}
+
 int scaling(int input, int *boundary, int *scale) {
 	int result = 0;
-	int test = 0; 
+	int low_numerator, high_numerator, difference = 0;
+	//int test = 0; 
 	if(input < boundary[0]) {
 		result = (input * scale[0]) / 100;
 	}
@@ -147,15 +189,19 @@ int scaling(int input, int *boundary, int *scale) {
 	for(int i = 1; i < 28; i++) {
 		if(input >= boundary[i-1] && input < boundary[i]) {
 			result = (input * ((scale[i-1] + scale[i])/2)) / 100; 
-			test = ((scale[i-1] + scale[i])/2);
+			low_numerator = (input - boundary[i-1]);
+			high_numerator = ((boundary[i] - input));
+			difference = boundary[i] - boundary[i-1]; 
+			result = ((low_numerator * scale[i-1]) + (high_numerator * scale[i])) / difference;
+			//test = ((scale[i-1] + scale[i])/2);
 		}			
 	}
 	if(input >= boundary[27]) {
 		result = (input * scale[27]) / 100;
-		test = 27;
+		//test = 27;
 	}
 	
-	return test;
+	return result;
 }	
 
 int sqrt(int input) {
@@ -186,7 +232,9 @@ void ADC_Print(void) {
 void ADC_PrintJSON(void) {
 	xSemaphoreTake(g_pUARTSemaphore, portMAX_DELAY);
 	//UARTprintf("@{\"pv\" : %d, \"inverter\" : %d, \"wind\" : %d, \"grid\" : %d, \"load\" : %d,}\n", 0, 0, 0, 0, load_v_rms);
-	UARTprintf("@{grid: \"load\" : %d, test %d}\n", test, load_v_rms);
+	//UARTprintf("@{grid: \"load\" : %d}\n", (load_i_rms * load_v_rms)/1000);
+	UARTprintf("@{grid: \"load\" : %d}\n", load_i_rms);
+	//UARTprintf("@{grid: \"load\" : %d, test %d}\n", load_v_rms, test);
 	xSemaphoreGive(g_pUARTSemaphore);
 }
 
@@ -217,8 +265,8 @@ uint32_t ADCTaskInit(void(*pTask))
 		SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER2);
-    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0); //must have enabled ADC first
-    GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0); //must have enabled ADC first
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_5 | GPIO_PIN_4 | GPIO_PIN_3 | GPIO_PIN_2 | GPIO_PIN_1 | GPIO_PIN_0); //must have enabled ADC first
+    GPIOPinTypeADC(GPIO_PORTD_BASE, GPIO_PIN_3 | GPIO_PIN_2 ); //must have enabled ADC first
 		
 		// timer 2 associated with ADC 0
 		TimerDisable(TIMER2_BASE, TIMER_A);
@@ -333,7 +381,6 @@ void ADC0Seq0_Handler(void)
 		ADCSequenceDataGet(ADC0_BASE, ADC_SEQUENCE0, &adcRawSS0Input[adc_ss0_index].PD2);
 		ADCSequenceDataGet(ADC0_BASE, ADC_SEQUENCE0, &adcRawSS0Input[adc_ss0_index].PE5);
     ADCSequenceDataGet(ADC0_BASE, ADC_SEQUENCE0, &adcRawSS0Input[adc_ss0_index].PE4);
-		
 	
 		//ADCSequenceDataGet(ADC0_BASE, ADC_SEQUENCE0, &test);
 		//UARTprintf("PD3: %d\n", test);
