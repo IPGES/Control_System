5633bd7210b8e14be91d45c43464ddb8b2482e7b
Unknown <joshua.graham@utexas.edu>
2018-04-16 16:00:50 -0500

    Changes

--- IPGES/Embedded/IPGESControlSystem/adc_task.c
+++ IPGES/Embedded/IPGESControlSystem/adc_task.c
@@ -136,7 +136,7 @@ static void ADCTask(void *pvParameters)
 					scb_mean_load_irms = avg_sum_load_irms/ARRAY_SIZE;
 					scb_mean_dist_vrms = avg_sum_dist_vrms/ARRAY_SIZE;
 					scb_mean_dist_irms = avg_sum_dist_irms/ARRAY_SIZE; 
-					UARTprintf("AVG: %d\n",scb_mean_load_vrms);
+					//UARTprintf("AVG: %d\n",scb_mean_load_vrms);
           for(int i = 0; i < ARRAY_SIZE; i++) {
 						//PE0
 						shifted_adc.PE0 = adcRawSS0Input[i].PE0 - scb_mean_load_vrms; //5.4 -> 1.63 mean 
@@ -151,7 +151,7 @@ static void ADCTask(void *pvParameters)
 						shifted_adc.PE3 = adcRawSS0Input[i].PE3 - scb_mean_dist_irms;
 						sum_dist_irms += shifted_adc.PE3 * shifted_adc.PE3; 
 						
-						UARTprintf("%d,", (adcRawSS0Input[i].PE0* 3300) / 4095);
+						//UARTprintf("%d,", (adcRawSS0Input[i].PE0* 3300) / 4095);
 						//UARTprintf("%d,", adcRawSS0Input[i].PE0);
 						//UARTprintf("%d,",shifted_adc.PE0);
           }
@@ -173,7 +173,7 @@ static void ADCTask(void *pvParameters)
 					load_v_rms = undo_signal_conditioning_load_vrms(result_load_vrms);
 			 		load_i_rms = undo_signal_conditioning_load_irms(result_load_irms);
 					dist_v_rms = undo_signal_conditioning_dist_vrms(result_dist_vrms);
-					dist_i_rms = undo_signal_conditioning_load_irms(result_dist_irms);
+					dist_i_rms = undo_signal_conditioning_dist_irms(result_dist_irms);
 					
 					//UARTprintf("Volt: %d\n", undo_signal_conditioning_load_vrms(23));
 					//UARTprintf("Curr: %d\n", result_dist_irms);
@@ -186,7 +186,7 @@ static void ADCTask(void *pvParameters)
 					UARTprintf("V-Load: %d\n", result_load_vrms);
 					UARTprintf("I-Load: %d\n", result_load_irms);			
 					*/
-					/*
+					/*	
 					UARTprintf("V-Dist: %d\n", load_v_rms); // JUlia uncommented this and the following line
 					UARTprintf("I-Dist: %d\n", load_i_rms);
 					UARTprintf("V-Load: %d\n", dist_v_rms);
@@ -249,13 +249,13 @@ int scaling(int input, int *boundary, int *scale) {
 	for(int i = 1; i < 28; i++) {
 		if(input >= boundary[i-1] && input < boundary[i]) {
 			//result = (input * scale[i-1]) / 100; //round down
-			//result = (input * ((scale[i-1] + scale[i])/2)) / 100; //avg
-			
+			result = (input * ((scale[i-1] + scale[i])/2)) / 100; //avg
+			/*
 			low_numerator = (input - boundary[i-1]);
 			high_numerator = ((boundary[i] - input));
 			difference = boundary[i] - boundary[i-1]; 
 			result = ((low_numerator * scale[i-1]) + (high_numerator * scale[i])) / difference;
-			
+			*/
 			//test = ((scale[i-1] + scale[i])/2);
 		}			
 	}
@@ -294,7 +294,7 @@ void ADC_Print(void) {
 
 void ADC_PrintJSON(void) {
 	//xSemaphoreTake(g_pUARTSemaphore, portMAX_DELAY);
-	//UARTprintf("@{\"pv\" : %d, \"inverter\" : %d, \"wind\" : %d, \"grid\" : %d, \"load\" : %d,}\n", 0, load_v_rms, load_i_rms, dist_v_rms, dist_i_rms);
+	UARTprintf("@{\"pv\" : %d, \"inverter\" : %d, \"wind\" : %d, \"grid\" : %d, \"load\" : %d,}\n", 0, load_v_rms, load_i_rms, dist_v_rms, dist_i_rms);
 	//UARTprintf("@{\"Load Volt\" : %d, \"Load Curr\" : %d, \"Dist Volt\" : %d, \"Dist Curr\" : %d,}\n", load_v_rms, load_i_rms, dist_v_rms, dist_i_rms);
 	//UARTprintf("@{grid: \"load\" : %d}\n", (load_i_rms * load_v_rms)/1000);
 	//UARTprintf("@{grid: \"load\" : %d}\n", load_v_rms);
